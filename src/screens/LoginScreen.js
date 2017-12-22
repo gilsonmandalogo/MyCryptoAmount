df81134e1966d70628a4a00f15b00bf5b940e58c
Gilson Marquato Júnior <gilson@mapprospect.com>
2017-12-22 01:36:57 -0200

    Added functions to create, recover, and login users

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableNativeFeedback, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableNativeFeedback, ScrollView, TextInput, Alert } from 'react-native';
 import { NavigationActions } from 'react-navigation';
+import firebase from 'firebase';
+import initializeFirebase from '../initializeFirebase';
 
 export default class LoginScreen extends React.Component {
   static navigationOptions = {
@@ -13,13 +15,50 @@ export default class LoginScreen extends React.Component {
     this.state = { email: '', password: '' };
   };
 
-  login = () => {
+  goHome = () => {
     const resetAction = NavigationActions.reset({
       index: 0,
       actions: [NavigationActions.navigate({ routeName: 'HomeScreen' })]
     });
 
     this.props.navigation.dispatch(resetAction);
+  };
+
+  login = () => {
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then(() => {
+      if (firebase.auth().currentUser.emailVerified) {
+        this.goHome();
+      } else {
+        Alert.alert('', 'Please verify your email address, check your mailbox');
+      };
+    })
+    .catch(error => {
+      Alert.alert('', error.message);
+    });
+  };
+
+  createAccount = () => {
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .then(() => {
+      firebase.auth().onAuthStateChanged(user => {
+        user.sendEmailVerification();
+        Alert.alert('', 'You have received an email with the activation link');
+      });
+    })
+    .catch(error => {
+      Alert.alert('', error.message);
+    });
+  };
+
+  forgotPassword = () => {
+    firebase.auth().sendPasswordResetEmail(this.state.email)
+    .then(() => {
+      Alert.alert('', 'You have received an email with the reset link');
+    })
+    .catch(error => {
+      Alert.alert('', error.message);
+    });
   };
 
   render() {
@@ -29,7 +68,7 @@ export default class LoginScreen extends React.Component {
         style={styles.scroll}
         centerContent        
       >
-        <Text style={styles.text} >E-mail</Text>
+        <Text style={styles.text} >Email</Text>
         <TextInput
           placeholder="Example@domain.com"
           onChangeText={email => this.setState( {email} )}
@@ -53,15 +92,41 @@ export default class LoginScreen extends React.Component {
 
         <TouchableNativeFeedback        
           background={TouchableNativeFeedback.Ripple('#00D900')}
-          onPress={() => this.login()}
-          secureTextEntry
+          onPress={() => this.login()}          
         >
-          <View style={styles.login} >
-            <Text style={styles.txtLogin} >Login</Text>
+          <View style={styles.buttons} >
+            <Text style={styles.txtButtons} >Login</Text>
+          </View>
+        </TouchableNativeFeedback>
+
+        <View style={styles.space} />
+
+        <TouchableNativeFeedback        
+          background={TouchableNativeFeedback.Ripple('#00D900')}
+          onPress={() => this.createAccount()}
+        >
+          <View style={styles.buttons} >
+            <Text style={styles.txtButtons} >Create account</Text>
+          </View>
+        </TouchableNativeFeedback>
+
+        <View style={styles.space} />
+
+        <TouchableNativeFeedback        
+          background={TouchableNativeFeedback.Ripple('#00D900')}
+          onPress={() => this.forgotPassword()}
+        >
+          <View style={styles.buttons} >
+            <Text style={styles.txtButtons} >Forgot password</Text>
           </View>
         </TouchableNativeFeedback>
       </ScrollView>
     );
+  };
+
+  componentDidMount() {
+    //This code haven't committed to github to protect the database access, anyway, here comes the default firebase.initializeApp from the firebase docs
+    initializeFirebase();
   };
 };
 
@@ -72,12 +137,12 @@ const styles = StyleSheet.create({
   scroll: {
     flex: 1,
   },
-  txtLogin: {
+  txtButtons: {
     textAlign: 'center',
     fontSize: 16,
     color: '#003D00',
   },
-  login: {
+  buttons: {
     flex: 1,
     borderColor: '#00D900',
     borderWidth: 1,
@@ -87,5 +152,8 @@ const styles = StyleSheet.create({
   text: {
     color: '#003D00',
     fontSize: 18,
+  },
+  space: {
+    marginBottom: 8,
   },
 });
